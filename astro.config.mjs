c1c5e4d9ed6a975827ea278db2118e02ebfab47b
Furkan Taşkın <furkan.taskin@tuta.io>
2025-03-27 17:59:36 +0300

    Modify home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,37 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Personal Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Personal Docs',
+          social: {
+              github: 'https://github.com/furkantaskin/wiki',
+              "x.com": 'https://x.com/muffinisamuffin',
+              instagram: 'https://instagram.com/muffinisamuffin',
+              linkedin: 'https://linkedin.com/in/furkant1',
+          },
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+		  customCss: ['./src/styles/global.css']
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
